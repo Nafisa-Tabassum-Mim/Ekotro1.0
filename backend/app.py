455d6a462a32be58ea44b4ed8058f2e50399bfbb
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2025-09-08 09:06:55 +0600

    delete sa added

--- backend/app.py
+++ backend/app.py
@@ -193,34 +193,9 @@ def token_required(f):
 
         except Exception as e:
             return jsonify({"error": str(e)})
-        return f(login_user["email"], *args, **kwargs)
+        return f(login_user["user_id"], *args, **kwargs)
 
     return decorator
-
-
-# @app.route("/", methods=["GET"])
-# @token_required
-# def home(login_user):
-#     print("login user email", login_user)
-#     return jsonify({"message": "Hello world"})
-
-
-# Code for jwt login and register
-
-# @app.route("/user", methods=["GET"])
-# @token_required
-# def get_user(email):
-#     cursor = conn.cursor(dictionary=True)
-#     cursor.execute(
-#         "SELECT name, email, fb_link, role, photo FROM user WHERE email = %s",
-#         (email,)
-#     )
-#     user = cursor.fetchone()
-#     cursor.close()
-
-#     if not user:
-#         return jsonify({"error": "User not found"}), 404
-#     return jsonify(user)
 
 
 # student part
@@ -228,15 +203,15 @@ def token_required(f):
 
 @app.route("/student_user", methods=["GET"])
 @token_required
-def get_student_user(email):
+def get_student_user(user_id):
     cursor = conn.cursor(dictionary=True)
     cursor.execute(
         """
         SELECT u.user_id, u.name, u.email, u.fb_link, u.role, u.photo,s.front_side,s.back_side,s.uni_name, s.dept FROM user u
         JOIN student s ON u.user_id = s.s_id
-        WHERE u.email = %s
+        WHERE u.user_id = %s
     """,
-        (email,),
+        (user_id,),
     )
 
     user_student = cursor.fetchone()
@@ -418,6 +393,167 @@ def add_to_wishlist():
         return jsonify({"error": str(e)}), 500
     finally:
         cursor.close()
+
+
+@app.route('/myWishlist', methods=['GET'])
+@token_required
+def get_wishlist(user_id):
+    cursor = conn.cursor(dictionary=True)  
+    query = """
+        SELECT e.event_id, e.name, e.uni_name
+        FROM Event e 
+        JOIN stu_in_event s ON s.event_id = e.event_id
+        WHERE s.ss_userID = %s
+    """
+    cursor.execute(query, (user_id,))
+    events = cursor.fetchall()
+    cursor.close()
+    return jsonify(events)
+
+# delete wishlist 
+
+@app.route('/wishlist/<int:event_id>', methods=['DELETE'])
+@token_required
+def delete_wishlist_item(user_id, event_id):
+    try:
+        cursor = conn.cursor() 
+        cursor.execute(
+            "DELETE FROM stu_in_event WHERE ss_userID = %s AND event_id = %s",
+            (user_id, event_id)
+        )
+        conn.commit()
+        cursor.close()
+        return jsonify({"message": "Event removed from wishlist successfully!"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# add event 
+
+@app.route('/create_event', methods=['POST'])
+@token_required
+def create_event(user_id):
+    data = request.get_json()
+
+    name = data.get("name")
+    description = data.get("description")
+    weblink = data.get("weblink")
+    fb_link = data.get("fb_link")
+    deadline = data.get("deadline")
+    prize_money = data.get("prize_money")
+    uni_name = data.get("uni_name")
+    location = data.get("location")
+
+    try:
+        cursor = conn.cursor()
+        query = """
+            INSERT INTO Event 
+            (name, description, weblink, fb_link, deadline, prize_money, uni_name, location, asa_userID)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""" 
+        values = (
+            name, description, weblink, fb_link, deadline,
+            prize_money, uni_name, location, user_id)
+
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+
+        return jsonify({"message": "Event created successfully!"}), 201
+
+    except Exception as e:
+        print("Error inserting event:", str(e))
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/event/<int:event_id>", methods=["DELETE"])
+def delete_event( event_id):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM Event WHERE event_id = %s", (event_id,))
+        conn.commit()
+        cursor.close()
+
+        return jsonify({"message": "Event removed successfully!"}), 200
+    
+    except Exception as e:
+        print("Error deleting event:", str(e))
+        return jsonify({"error": str(e)}), 500
+    
+# funding 
+@app.route("/apply_funding/<int:event_id>", methods=["POST"])
+@token_required
+def apply_funding(user_id,event_id):
+    data = request.get_json()
+    type_ = data.get("type")
+    amount = data.get("amount")
+
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO funding (type, amount, event_id, asa_userid)
+            VALUES (%s, %s, %s, %s)
+            """,
+            (type_, amount, event_id, user_id)
+        )
+        conn.commit()
+        cursor.close()
+        return jsonify({"message": "Funding applied successfully!"}), 201
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({"error": str(e)}), 500
+
+@app.route("/funding_requests", methods=["GET"])
+def funding_requests():
+    cursor = conn.cursor(dictionary=True)
+    query = """
+        SELECT f.fund_id, f.type, f.amount, f.event_id, e.name AS event_name, e.uni_name
+        FROM Funding f
+        JOIN Event e ON f.event_id = e.event_id
+    """
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return jsonify(data)
+
+# student_ambassador 
+
+@app.route("/get_sas", methods=["GET"])
+def get_sas():
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT s.*, u.name, u.email,u.fb_link,u.role FROM student_ambassador s JOIN user u ON s.sa_id = u.user_id")
+    sas = cursor.fetchall()
+    cursor.close()
+    return jsonify(sas)
+
+
+@app.route("/approve_sa/<int:user_id>", methods=["POST"])
+def approve_sa(user_id):
+    try:
+        cursor = conn.cursor()
+        # Update role in user table
+        cursor.execute("UPDATE user SET role='student_ambassador' WHERE user_id=%s", (user_id,))
+        conn.commit()
+        cursor.close()
+        return jsonify({"message": "Student Ambassador approved successfully!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
+@app.route("/delete_sa/<int:sa_id>", methods=["DELETE"])
+def delete_sa(sa_id):
+    try:
+        cursor = conn.cursor()
+        # Delete from student_ambassador table
+        cursor.execute("DELETE FROM student_ambassador WHERE sa_id=%s", (sa_id,))
+        # Change role back to student
+        cursor.execute("UPDATE user SET role='student' WHERE user_id=%s", (sa_id,))
+        conn.commit()
+        cursor.close()
+        return jsonify({"message": "Student Ambassador deleted successfully!"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
